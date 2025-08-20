47e137c3dbe536c18299a9c5b8a8136d70187612
kiarash-naderi <kiarashnaaderi@gmail.com>
2025-08-20 19:04:46 +0330

    increase token expiry time

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,5 +2,5 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const JWT_SECRET = process.env.JWT_SECRET ?? "dev_secret_change_me";
-export const JWT_EXPIRES = process.env.JWT_EXPIRES ?? "15m";
+export const JWT_EXPIRES = process.env.JWT_EXPIRES ?? "1h";
 export const PORT = Number(process.env.PORT || 3000);
