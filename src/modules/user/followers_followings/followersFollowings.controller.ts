7415f6e1c92326d7d408becd6780b6c5187c4ee7
a.yousefi <a.yousefi@khodro45.com>
2025-09-04 23:48:05 +0330

    feat(followers): add handlers for retrieving followers and followings

---
+++ src/modules/user/followers_followings/followersFollowings.controller.ts
@@ -0,0 +1,31 @@
+import { AuthRequest } from '../../auth/auth.middleware';
+import { Response } from 'express';
+import { getFollowers, getFollowings } from './followersFollowings.service';
+import { FollowListApiResponse } from './followersFollowings.types';
+import { handleError } from '../../../utils/errorHandler';
+
+export async function getFollowersHandler(req: AuthRequest, res: Response<FollowListApiResponse>) {
+    try {
+        const followers = await getFollowers(req.user!.id);
+        return res.json({
+            success: true,
+            message: 'لیست فالوورها با موفقیت دریافت شد',
+            data: followers,
+        });
+    } catch (error) {
+        return handleError(error, res, 'خطا در دریافت لیست فالوورها');
+    }
+}
+
+export async function getFollowingsHandler(req: AuthRequest, res: Response<FollowListApiResponse>) {
+    try {
+        const followings = await getFollowings(req.user!.id);
+        return res.json({
+            success: true,
+            message: 'لیست فالووینگ‌ها با موفقیت دریافت شد',
+            data: followings,
+        });
+    } catch (error) {
+        return handleError(error, res, 'خطا در دریافت لیست فالووینگ‌ها');
+    }
+}
