b486f2972ecce98154be00608d831a1b4229a23c
kiarash-naderi <kiarashnaaderi@gmail.com>
2025-09-04 23:51:08 +0330

    add MinIO configuration example

--- src/config/minio.config.ts
+++ src/config/minio.config.ts
@@ -7,3 +7,17 @@ export const minioClient = new Minio.Client({
   accessKey: process.env.MINIO_ACCESS_KEY || "minioadmin",
   secretKey: process.env.MINIO_SECRET_KEY || "minioadmin",
 });
+
+
+
+// baraye estefade roye server
+
+// import Minio from 'minio';
+
+//   export const minioClient = new Minio.Client({
+//     endPoint: process.env.MINIO_ENDPOINT || 'your-server-ip', // تغییر از localhost
+//     port: parseInt(process.env.MINIO_PORT || '9000', 10),
+//     useSSL: process.env.MINIO_USE_SSL === 'true', // true برای سرور
+//     accessKey: process.env.MINIO_ACCESS_KEY ,
+//     secretKey: process.env.MINIO_SECRET_KEY ,
+//   });
